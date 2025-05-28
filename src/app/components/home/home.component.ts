c45a0f6e7975517cf8fd0f15bd6ff6855228c2d5
ViniShuet <vinishuet@gmail.com>
2025-05-28 19:36:27 -0300

    Interpolacao e Control Flow

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+exibir = true;
+titulo = "Global Solution";
+descricao = "Uma jornada tecnológica rumo ao futuro";
+dataEvento = "26 de maio";
+destaques = ["Inovacao", "Tecnologia", "Impacto Social"];
+
+exibirInformacoes() {
+  this.exibir = !this.exibir;
+}
 
 }
