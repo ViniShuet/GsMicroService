d48dd27f3a24f9c435ea43423c0780d8bb562975
ViniShuet <vinishuet@gmail.com>
2025-05-28 19:22:27 -0300

    First commit add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { GiftsComponent } from './components/gifts/gifts.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { InspiracaoComponent } from './components/inspiracao/inspiracao.component';
+import { ParceirosComponent } from './components/parceiros/parceiros.component';
+import { ProgramacaoComponent } from './components/programacao/programacao.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    FooterComponent,
+    GiftsComponent,
+    HeaderComponent,
+    HomeComponent,
+    InspiracaoComponent,
+    ParceirosComponent,
+    ProgramacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
