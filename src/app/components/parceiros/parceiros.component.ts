ff0dcc8fec52c125ccfa92003dacaa2a2f6ad855
ViniShuet <vinishuet@gmail.com>
2025-05-28 19:50:54 -0300

    Property Binding

--- src/app/components/parceiros/parceiros.component.ts
+++ src/app/components/parceiros/parceiros.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './parceiros.component.css'
 })
 export class ParceirosComponent {
+  logoIBM = "../../../assets/IBM_logo.svg.png"
+  logoMedico = "../../../assets/medico.svg.png"
+  logoINPE = "../../../assets/Inpelogo.png"
+  logoSantoAndre = "../../../assets/logo-santoandre.png"
+  logoSaoPauloDef = "../../../assets/Brasão_CBM_SP.png"
 
 }
